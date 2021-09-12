ef5171d7f99c3a3a1bd72f776d530410a3d909f6
amandatorresapu <69279617+amandatorresapu@users.noreply.github.com>
2021-09-11 20:40:34 -0700

    added git hub repo and link for Amanda

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <p>
+        This project is created by{" "}
+        <a
+          href="https://www.linkedin.com/in/amanda-torres-brooks-6826b15a/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Amanda Torres
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/amandatorresapu/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open sourced on GitHub
+        </a>
+      </p>
+    </div>
+  );
 }
